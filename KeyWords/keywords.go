2fa285924b4ea10a25a374c44f2126ad3de9d502
amreshsriram <amreshvsriram45@gmail.com>
2022-04-22 13:21:20 +0530

    Commit 2

---
+++ KeyWords/keywords.go
@@ -0,0 +1,14 @@
+// Go program to illustrate the
+// use of keywords
+package main
+
+import "fmt"
+
+// Here, package, import, func,
+// var are keywords
+func main() {
+
+	var a = "Welcome to the GO programming language"
+
+	fmt.Println(a)
+}
